7f161d7cc002497f4a72faba1ac3dd861ba01480
rachid j <2203618@afpa.fr>
2023-01-20 12:03:30 +0100

    feat: suppression des articles

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -604,23 +604,45 @@ const displayArticles = articles => {
             <p class="article-author">${article.author}</p>
             <p class="article-content">${article.content}</p>
             <div class="article-actions">
-              <button class="btn btn-danger" data.id=${article._id} >Supprimer</button>
+              <button class="btn btn-danger" data-id=${article._id} >Supprimer</button>
             </div>
           `;
     return articleNode;
   });
-  console.log(articlesDOM);
   articlesContainer.innerHTML = "";
   articlesContainer.append(...articlesDOM);
+  const deleteBtns = articlesContainer.querySelectorAll(".btn-danger");
+  deleteBtns.forEach(button => {
+    button.addEventListener("click", async event => {
+      event.preventDefault();
+      try {
+        const target = event.target;
+        console.log(target);
+        const articleId = target.dataset.id;
+        const response = await fetch(`https://restapi.fr/api/dwwm_rachid/${articleId}`, {
+          method: "DELETE"
+        });
+        const body = await response.json();
+        fetchArticles();
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  });
 };
 const fetchArticles = async () => {
+  // fonction asynchrone qui recupere les donnees depuis l'API
   try {
     const response = await fetch("https://restapi.fr/api/dwwm_rachid");
     const articles = await response.json();
-    if (!articles.length) {
-      displayArticles([articles]);
+    if (articles.length !== 0) {
+      if (!articles.length) {
+        displayArticles([articles]);
+      } else {
+        displayArticles(articles);
+      }
     } else {
-      displayArticles(articles);
+      articlesContainer.innerHTML = "<p>Pas d'articles...<p>";
     }
   } catch (error) {
     console.log(error);
